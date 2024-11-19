b477ab605bc905e4dfa778ebf24e28164020e0d9
riya-meghnani <riyameghnani2311@gmail.com>
2024-11-20 00:15:36 +0530

    added test and changes for handling sum for comma separated input

--- string-calc.js
+++ string-calc.js
@@ -2,5 +2,14 @@ export function add(numbers) {
     if (numbers === "") {
       return 0;
     }
+    else {
+        let no = numbers.split(",")
+        if(no.length == 1){
+            return 1
+        }
+        else {
+            return no.reduce((acc, element) => acc = acc + parseInt(element), 0);
+        }
+    }
     return null; 
   }
--- string-calc.test.js
+++ string-calc.test.js
@@ -3,3 +3,11 @@ import { add } from "./string-calc";
 test('empty string test', ()=>{
     expect(add("")).toBe(0);
 })
+
+test("single number returns the number itself", () => {
+    expect(add("1")).toBe(1);
+});
+
+test("comma separated value should return sum", () => {
+    expect(add("1,2,3")).toBe(6);
+});
